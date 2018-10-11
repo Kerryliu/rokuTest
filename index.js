0aef57c567ca6bfc0e074e7bb289a061e9c62474
Kerry Liu <kerryliu123@gmail.com>
2018-10-10 23:20:41 -0700

    Adding localtunnel

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import http from 'http';
+import localtunnel from 'localtunnel';
 
-const app = express()
-const port = 3000
-const rokuHostname = "192.168.1.42"
-const rokuPort = 8060
+const app = express();
+const port = 3000;
+const rokuHostname = "192.168.1.42";
+const rokuPort = 8060;
+const localtunnelOpts = {
+    subdomain: 'berrykerryisthebest'
+}
 
 function createPost(path) {
     return {
@@ -16,6 +20,7 @@ function createPost(path) {
 }
 
 function pokeRoku(res, path, userResponseText) {
+    let retryCount = 0;
     const rokuReq = http.request(createPost(path), (rokuRes) => {
         console.log('RokuStatusCode:', rokuRes.statusCode);
         console.log(userResponseText);
@@ -23,11 +28,25 @@ function pokeRoku(res, path, userResponseText) {
         res.send(userResponseText);
     });
 
+    rokuReq.setTimeout(5000);
     rokuReq.write('');
+    rokuReq.on('error', () => {
+        retryCount++;
+        if(retryCount < 5) {
+            rokuReq.write('');
+        }
+    });
     rokuReq.end();
 }
 
 app.get('/poweron', (req, res) => pokeRoku(res, '/keypress/PowerOn', 'Hopefully TV turned on.'));
 app.get('/poweroff', (req, res) => pokeRoku(res, '/keypress/PowerOff', 'Hopefully TV turned off.'));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const tunnel = localtunnel(port, localtunnelOpts, (err, tunnel) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log("localtunnel URL:", tunnel.url);
+
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+});
